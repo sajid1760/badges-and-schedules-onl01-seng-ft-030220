8d20ee564caef56b11d9505dc1c94a5fdb83980b
Sajid Anjum <sajid1760@gmail.com>
2020-03-09 00:02:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -28,13 +28,9 @@ def printer
   assignments = []
   names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   badges = batch_badge_creator(names) 
-  badges.each do |badge|
-    puts badge
-  end
+  
   assignments = assign_rooms(names)
-  assignments.each do |assignment|
-    puts assignment
-  end
+  
 end
  
 printer
